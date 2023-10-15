01eef69046a54732f74eb23e4e22e8b1f4e0f44b
AhmedEisa <AhmedEisa@DESKTOP-O9V8KLJ>
2023-10-16 01:05:58 +0300

    7 models without navigations

---
+++ RestaurantAPI/Models/RecipeFeedback.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class RecipeFeedback
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Feedback text is required.")]
+        public string text { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Rate must be between 1 and 5.")]
+        public decimal Rate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Post Date")]
+        public DateTime PostDate { get; set; }
+    }
+}
