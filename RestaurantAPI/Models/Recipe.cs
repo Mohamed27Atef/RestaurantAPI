8b66d7bd3a1c609f1f7ce5ac6b76a5bde573b341
AhmedEisa <AhmedEisa@DESKTOP-O9V8KLJ>
2023-10-15 09:48:48 +0300

    change from product to recipe

---
+++ RestaurantAPI/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantAPI.Models
+{
+    public class Recipe
+    {
+        public int id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string name { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+
+
+    }
+}
