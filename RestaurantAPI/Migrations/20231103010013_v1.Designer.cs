00888b75f3a8a0930e8f61fbd20af2dcfe932435
AhmedEisa <AhmedEisa@DESKTOP-O9V8KLJ>
2023-11-04 18:14:57 +0200

    .

--- RestaurantAPI/Migrations/20231103010013_v1.Designer.cs
+++ RestaurantAPI/Migrations/20231103010013_v1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantAPI.Models;
 
@@ -11,9 +12,11 @@ using RestaurantAPI.Models;
 namespace RestaurantAPI.Migrations
 {
     [DbContext(typeof(RestaurantContext))]
-    partial class RestaurantContextModelSnapshot : ModelSnapshot
+    [Migration("20231103010013_v1")]
+    partial class v1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -663,10 +666,6 @@ namespace RestaurantAPI.Migrations
 
                     b.Property<decimal>("OpenHours")
                         .HasColumnType("decimal(18,2)");
-
-                    b.Property<string>("Password")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<decimal?>("Rate")
                         .HasColumnType("decimal(18,2)");
---
+++ RestaurantAPI/Migrations/20231103010013_v1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Copons_coponid",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_coponid",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "coponid",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Resturants",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Resturants");
+
+            migrationBuilder.AddColumn<int>(
+                name: "coponid",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_coponid",
+                table: "Orders",
+                column: "coponid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Copons_coponid",
+                table: "Orders",
+                column: "coponid",
+                principalTable: "Copons",
+                principalColumn: "id");
+        }
+    }
+}
